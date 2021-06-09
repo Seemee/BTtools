b25924cf24c25ea96c8365842637a27de5cac131
Symen Hovinga <itsfull@hotmail.com>
2021-06-09 22:40:06 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,5 +23,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["gzip", "math","pathlib","lxml","copy","numpy"]
+    install_requires=["lxml","numpy"]
 )
