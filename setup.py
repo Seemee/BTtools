57656f86a24ec1002456495c354a19b3c7a4ee32
Symen Hovinga <itsfull@hotmail.com>
2022-06-10 10:34:43 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="BTtools",
-    version="V6.48",
+    version="V6.49",
     author="Symen Hovinga",
     author_email="itsfull@hotmail.com",
     description="Tools for processing Burrtools xmpuzzle files in Python",
